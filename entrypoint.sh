3e9d7bd9dd6e2d2d1dbe3ae10058020099be82e9
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-09-15 17:36:09 +0200

    Added comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 set -e
 
-
+# check if run with default arguments
 if [ "$1" = '/usr/bin/supervisord' ]; then
-    if [ -z ${GIT_CLONE_URL+x} ]; then 
-        echo "No git repository URL to clone specified, can't serve content"
-        echo "Please provide GIT_CLONE_URL environment variable"
-        exit
-    fi
+  if [ -z ${GIT_CLONE_URL+x} ]; then
+    # display error on missing variable & exit
+    echo "No git repository URL to clone specified, can't serve content"
+    echo "Please provide GIT_CLONE_URL environment variable"
+    exit
+  fi
 
-    rm -rf "${FS_SRV}/htdocs"
-    git clone $GIT_CLONE_URL "${FS_SRV}/htdocs"
-    cp "${FS_SRV}/index.html" "${FS_SRV}/htdocs"
+  # clean working directory
+  rm -rf "${FS_SRV}/htdocs"
+  # clone provided repository
+  git clone $GIT_CLONE_URL "${FS_SRV}/htdocs"
+  # patch MDwiki into serving folder
+  cp "${FS_SRV}/index.html" "${FS_SRV}/htdocs"
+  # patch webhook properties template
+  envsubst < "${FS_OPT}/hooks.json.tpl" > "${FS_OPT}/hooks.json"
 fi
 
-envsubst < "${FS_OPT}/hooks.json.tpl" > "${FS_OPT}/hooks.json"
-
+# run default command (supervisord)
 exec "$@"
